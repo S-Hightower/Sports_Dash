6626ad58251948c4a3f0b8bac6e401c66c39240e
Shawna Hightower <sdhightower6@gmail.com>
2022-02-17 14:09:17 -0700

    removing extra folders/files

--- flask_app/controllers/controller_data.py
+++ flask_app/controllers/controller_data.py
@@ -1,9 +1,10 @@
 # controller handles routing and rendering templates
 
+from crypt import methods
 from flask import render_template, redirect, request, session, flash
 from flask_app import app
 # from flask_app.models.model_data import Data
 
-@app.route('/')
-def livescores():
+@app.route('/', methods=['GET', 'POST'])
+def index():
     return render_template('test.html')
