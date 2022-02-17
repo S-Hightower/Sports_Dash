6acafb947513d0a00e1f6285450b00eacf0c7551
Shawna Hightower <sdhightower6@gmail.com>
2022-02-17 12:53:22 -0700

    more updates to optimize MVC structure

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,3 +1,3 @@
-from flask import Flask, render_template, redirect, session
+from flask import Flask
 app = Flask(__name__)
-app.secret_key='keep is secret, keep it safe'
+app.secret_key='keep it secret, keep it safe'
