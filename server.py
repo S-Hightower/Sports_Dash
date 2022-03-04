06667dd443e339ac062b6dbbc769f46beaea453a
Shawna Hightower <sdhightower6@gmail.com>
2022-03-04 14:27:10 -0700

    updates

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+import requests
+from requests import Session
+
+import secrets
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
