d21a01bf0bff019dd397502843e8ea9530ee017f
Shawna Hightower <sdhightower6@gmail.com>
2022-03-04 14:11:08 -0700

    redo -- issues

--- flask_app/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-app.secret_key='keep it secret, keep it safe'
--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from flask_app import app
-
-@app.route('/')
-def main():
-    return render_template('test.html')
-
-if __name__=="__main__":
-    app.run(debug=True)
